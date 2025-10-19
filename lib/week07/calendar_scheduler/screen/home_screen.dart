4b580c17d58a0a6bb777cc2434c8f3fd36b0801b
platypus-perry03 <choe31904@gmail.com>
2025-10-19 17:42:20 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/screen/home_screen.dart
+++ lib/week07/calendar_scheduler/screen/home_screen.dart
@@ -8,3 +8,71 @@ class HomeScreen exteds StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: PRIMARY_COLOR,
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            isDismissible: true,
+            builder: (_) => ScheduleBottomSheet(
+              selectedDate: selectedDate,
+            ),
+            isScrollControlled: true,
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+
+      body: SafeArea(
+        child: Column(
+          children: [
+            MainCalendar(
+              selectedDate: selectedDate,
+              onDaySelected: onDaySelected,
+            ),
+            const SizedBox(height: 8.0),
+            StreamBuilder<List<Schedule>>(
+              stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
+              builder: (context, snapshot) {
+                return TodayBanner(
+                  selectedDate: selectedDate,
+                  count: snapshot.data?.length ?? 0,
+                );
+              },
+            ),
+            const SizedBox(height: 8.0),
+            Expanded(
+              child: StreamBuilder<List<schedule>> (
+                steam: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const SizedBox.shrink();
+                  }
+
+                  final schedules = snapshot.data!;
+                  return ListView.builder(
+                    itemCount: schedules.length,
+                    itemBuilder: (context, index) {
+                      final schedule = schedules[index];
+                    }
+                  )
+                }
+              )
+            )
+          ]
+        )
+      )
+    )
+  }
+}

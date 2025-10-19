e744812a53d79ec1458d77d7008a5357da4bbab3
platypus-perry03 <choe31904@gmail.com>
2025-10-19 17:21:20 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/component/schedule_card.dart
+++ lib/week07/calendar_scheduler/component/schedule_card.dart
@@ -3,5 +3,24 @@ import 'package:myapp/week06/calendar_scheduler/database/drift_database.dart';
 import 'package:myapp/week06/calendar_scheduler/componet/today_banner.dart';
 
 class _HomeScreenState extends State<HomeScreen> {
-  
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton( // 새 일정 버튼
+        backgroundColor: PRIMARY_COLOR,
+        onPressed: () {
+          showModalBottomSheet( // BottomSheet 열기
+            context: context,
+            isDismissible: true, // 배경 탭 했을 때 BoottomSheet 닫기
+            builder: (_) => ScheduleBottomSheet(
+              selectedDate: selectedDate,
+            ),
+            // BottomSheet의 높이를 화면의 최대 높이로 정의하고 스크롤 가능하게 변경
+          )
+        }
+      )
+    )
+  }
 }

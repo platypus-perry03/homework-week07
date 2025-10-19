0b7e2a3fca5c506f23e5209311d1b5836fc35fda
platypus-perry03 <choe31904@gmail.com>
2025-10-19 17:31:50 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/component/schedule_card.dart
+++ lib/week07/calendar_scheduler/component/schedule_card.dart
@@ -46,12 +46,58 @@ class _HomeScreenState extends State<HomeScreen> {
             const SizedBox(height: 8.0),
             Expanded(
               child: StreamBuilder<List<schedule>> (
-                stream: G
-              )
-            )
-          ]
-        )
-      )
-    )
+                stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const SizedBox.shrink();
+                  }
+
+                  final schedules = snapshot.data!;
+                  return ListView.builder(
+                    itemCount: schedules.length,
+                    itemBuilder: (context, index) {
+                      final schedule = schedules[index];
+
+                      return Dismissible(
+                        key: ObjectKey(schedule.id),
+                        direction: DismissDirection.startToEnd,
+                        onDismissed: (DismissDirection direction) {
+                          GetIt.I<LocalDatabase>().removeSchedule(schedule.id);
+                        },
+                        backgound: Container(
+                          color: Colors.red.withOpacity(0.1),
+                          alignment: Alignment.centerLeft,
+                          padding: const EdgeInset.symmetric(horizontal: 16),
+                          child: const Icon(Icons.delete),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            bottom: 8.0,
+                            left: 8.0,
+                            right: 8.0,
+                          ),
+                          child: ScheduleCard(
+                            startTime: schedule.startTime,
+                            endTime: schedule.endTime,
+                            content: schedule.context, // <- content로 사용
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
+    // 날짜 선택될 때마다 실행할 함수
+    setState(() {
+      this.selectedDate = selectedDate;
+    });
   }
 }

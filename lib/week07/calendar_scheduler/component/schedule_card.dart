c476023d36b5e527659f3f7984c4c6cbdef580bc
platypus-perry03 <choe31904@gmail.com>
2025-10-19 17:18:20 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/component/schedule_card.dart
+++ lib/week07/calendar_scheduler/component/schedule_card.dart
@@ -2,3 +2,6 @@ import 'package:flutter/gestures.dart';
 import 'package:myapp/week06/calendar_scheduler/database/drift_database.dart';
 import 'package:myapp/week06/calendar_scheduler/componet/today_banner.dart';
 
+class _HomeScreenState extends State<HomeScreen> {
+  
+}

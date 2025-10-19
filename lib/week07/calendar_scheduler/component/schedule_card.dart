5d3733b0c318e3ad1ecafeb58eb2dfa806910cd0
platypus-perry03 <choe31904@gmail.com>
2025-10-19 17:17:20 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/component/schedule_card.dart
+++ lib/week07/calendar_scheduler/component/schedule_card.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:myapp/week06/calendar_scheduler/database/drift_database.dart';
 import 'package:myapp/week06/calendar_scheduler/componet/today_banner.dart';
 

c44d0ee9fc206b9dec3bed8d92d984f85795f53c
platypus-perry03 <choe31904@gmail.com>
2025-10-19 17:23:20 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/component/schedule_card.dart
+++ lib/week07/calendar_scheduler/component/schedule_card.dart
@@ -18,8 +18,24 @@ class _HomeScreenState extends State<HomeScreen> {
               selectedDate: selectedDate,
             ),
             // BottomSheet의 높이를 화면의 최대 높이로 정의하고 스크롤 가능하게 변경
-          )
-        }
+            isScrollControlled: true,
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+
+      body: SafeArea( // 시스템 UI 피해서 UI 구현하기
+        child: Column( // 달력과 리스트를 세로로 배치
+          children: [
+            // 미리 작업해둔 달력 위젯 보여주기
+            MainCalendar(
+              selectedDate: selectedDate, // 선택된 날짜 전달하기
+              onDaySelected: onDaySelected, // 날짜가 선택됐을 때 실행할 함수
+            ),
+
+            
+          ]
+        )
       )
     )
   }

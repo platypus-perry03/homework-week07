810a406be2a2d9aaff104ef55db06fe60e160d21
platypus-perry03 <choe31904@gmail.com>
2025-10-19 17:34:50 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/screen/home_screen.dart
+++ lib/week07/calendar_scheduler/screen/home_screen.dart
@@ -1,2 +1,10 @@
 import 'package:myapp/week06/calendar_scheduler/component/schedule_bottom_sheet.dart';
 import 'package:myapp/week06/calendar_scheduler/component/today_banner.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen exteds StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}

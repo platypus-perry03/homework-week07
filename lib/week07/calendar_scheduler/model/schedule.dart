f8766032886b48402a5e4b28bd858e47d81db155
platypus-perry03 <choe31904@gmail.com>
2025-10-19 16:38:20 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/model/schedule.dart
+++ lib/week07/calendar_scheduler/model/schedule.dart
@@ -5,5 +5,5 @@ class Schedules extends Table {
   TextColumn get content => text()(); // 내용, 글자 열
   DateTimeColumn get date => dateTime()(); // 일정 날짜, 날짜 열
   IntColumn get startTime => integer()(); // 시작 시간
-  IntColumn get endTime => interger()(); // 종료 시간
+  IntColumn get endTime => integer()(); // 종료 시간
 }
